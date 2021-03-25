8f937dc7a36b645ca525a1896f4a7ce89695ede2
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-03-25 16:29:51 -0600

    Imported all components  to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,29 @@
+/* eslint-disable import/no-named-as-default */
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { StaticImage } from 'gatsby-plugin-image'
 
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Hero from '../components/hero'
+import Layout from '../components/layout'
+import SkillLogos from '../components/skills-logos'
+import ClientWork from '../components/client-work'
+import LearningProjects from '../components/learning-projects'
+import About from '../components/about'
+import Blog from '../components/blog'
+import Contact from '../components/contact'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Hero />
-  </Layout>
+  <>
+    <Layout>
+      <SEO title="Home" />
+      <Hero />
+      <SkillLogos />
+      <ClientWork />
+      <LearningProjects />
+      <About />
+      <Blog />
+      <Contact />
+    </Layout>
+  </>
 )
 
 export default IndexPage
