88ea0a2b1ddfb7b8f697888ddeb5b0e0b3f9333d
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-04 17:41:04 -0600

    Added nav item names

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,7 @@ const Header = () => (
         <>
           <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#" className="text-xl font-bold">
+              <a href="#" className="text-xl font-bold hover:text-gray-700">
                 Salym Akhmedov
               </a>
             </div>
@@ -45,25 +45,25 @@ const Header = () => (
                 href="#"
                 className="text-md font-medium text-gray-500 hover:text-gray-900"
               >
-                Solutions
+                Portfolio
               </a>
               <a
                 href="#"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
-                Pricing
+                About
               </a>
               <a
                 href="#"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
-                Partners
+                Blog
               </a>
               <a
                 href="#"
                 className="text-base font-medium text-gray-500 hover:text-gray-900"
               >
-                Company
+                Contact
               </a>
             </nav>
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
@@ -71,7 +71,7 @@ const Header = () => (
                 href="#"
                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
               >
-                Sign up
+                Hire me
               </a>
             </div>
           </div>
@@ -126,32 +126,39 @@ const Header = () => (
                     <Menu.Item
                       as="a"
                       href="#"
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50 ring-2"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:border-transparent "
                     >
-                      Product
+                      Portfolio
                     </Menu.Item>
                     <Menu.Item
                       as="a"
                       href="#"
                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
                     >
-                      Product
+                      About
                     </Menu.Item>
                     <Menu.Item
                       as="a"
                       href="#"
                       className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
                     >
-                      Product
+                      Blog
+                    </Menu.Item>
+                    <Menu.Item
+                      as="a"
+                      href="#"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                    >
+                      Contact
                     </Menu.Item>
                   </div>
                   <div className="mt-6 px-5">
                     <Menu.Item
                       as="a"
                       href="#"
-                      className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                      className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-0 focus:border-transparent "
                     >
-                      Start free trial
+                      Hire me
                     </Menu.Item>
                   </div>
                 </div>
@@ -165,161 +172,3 @@ const Header = () => (
 )
 
 export default Header
-
-/* <header>
-<div className="relative bg-white">
-  <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
-    <div className="flex justify-start lg:w-0 lg:flex-1">
-      <a href="#" className="text-xl font-bold">
-        Salym Akhmedov
-      </a>
-    </div>
-    <div className="-mr-2 -my-2 md:hidden">
-      <button
-        type="button"
-        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-cyan-400 hover:text-cyan-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500"
-        aria-expanded="false"
-      >
-        <span className="sr-only">Open menu</span>
-        <!-- Heroicon name: outline/menu -->
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      </button>
-    </div>
-    <nav className="hidden md:flex space-x-10">
-      <a
-        href="#"
-        className="text-md font-medium text-gray-500 hover:text-gray-900"
-      >
-        Solutions
-      </a>
-      <a
-        href="#"
-        className="text-base font-medium text-gray-500 hover:text-gray-900"
-      >
-        Pricing
-      </a>
-      <a
-        href="#"
-        className="text-base font-medium text-gray-500 hover:text-gray-900"
-      >
-        Partners
-      </a>
-      <a
-        href="#"
-        className="text-base font-medium text-gray-500 hover:text-gray-900"
-      >
-        Company
-      </a>
-    </nav>
-    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-      <a
-        href="#"
-        className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
-      >
-        Sign up
-      </a>
-    </div>
-  </div>
-  
-  <!--
-    Mobile menu, show/hide based on mobile menu state.
-
-    Entering: "duration-200 ease-out"
-      From: "opacity-0 scale-95"
-      To: "opacity-100 scale-100"
-    Leaving: "duration-100 ease-in"
-      From: "opacity-100 scale-100"
-      To: "opacity-0 scale-95"
-  -->
-  <div className="hidden absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-    <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-      <div className="px-5 pt-4 flex items-center justify-between">
-        <div>
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
-            alt=""
-          />
-        </div>
-        <div className="-mr-2">
-          <button
-            type="button"
-            className="bg-white rounded-md p-2 inline-flex items-center justify-center text-cyan-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600"
-          >
-            <span className="sr-only">Close menu</span>
-            <!-- Heroicon name: outline/x -->
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-      <div className="pt-5 pb-6">
-        <div className="px-2 space-y-1">
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-          >
-            Product
-          </a>
-
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-          >
-            Features
-          </a>
-
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-          >
-            Marketplace
-          </a>
-
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-          >
-            Company
-          </a>
-        </div>
-        <div className="mt-6 px-5">
-          <a
-            href="#"
-            className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
-          >
-            Start free trial
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</header> */
