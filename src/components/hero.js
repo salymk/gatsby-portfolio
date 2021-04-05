5f576ee4cdfb496c0cf8f8e7fc62beac1d9d64f4
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-05 17:29:54 -0600

    Added scroll effect

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link as LinkScroll } from 'react-scroll'
 
 export const Hero = () => (
   <>
-    <div className="relative">
+    <div className="relative mt-20 md:mt-0">
       <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
@@ -28,12 +29,32 @@ export const Hero = () => (
             </p>
             <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5">
-                <a
-                  href="#"
-                  className="uppercase flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-700 bg-white hover:bg-cyan-50 sm:px-8"
+                <LinkScroll
+                  to="tools"
+                  spy
+                  smooth
+                  offset={-100}
+                  duration={1000}
+                  className="cursor-pointer uppercase flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-700 bg-white hover:bg-cyan-50 sm:px-8"
                 >
                   Find out more
-                </a>
+                  <div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 ml-3 -mr-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 14l-7 7m0 0l-7-7m7 7V3"
+                      />
+                    </svg>
+                  </div>
+                </LinkScroll>
               </div>
             </div>
           </div>
