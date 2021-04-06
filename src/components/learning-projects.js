9bd59aa1ab63c97824602e9ed78b6486572afa9e
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-06 11:15:09 -0600

    Updated title and description

--- src/components/learning-projects.js
+++ src/components/learning-projects.js
@@ -11,12 +11,31 @@ const LearningProjects = () => {
       <div className="relative bg-white sm:py-8">
         <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
           <p className="mt-2 text-3xl font-extrabold text-cyan-500 tracking-tight sm:text-4xl">
-            Learning Projects
+            Bootcamp Projects
           </p>
           <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-            Phasellus lorem quam molestie id quisque diam aenean nulla in.
-            Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-            condimentum id viverra nulla.
+            Here are some of the bootcamp projects I completed from platforms
+            like
+            <a
+              href="https://teamtreehouse.com/techdegree/front-end-web-development"
+              target="_blank"
+              rel="noreferrer"
+              className="text-cyan-500 hover:text-cyan-600"
+            >
+              {' '}
+              TeamTreehouse{' '}
+            </a>
+            and{' '}
+            <a
+              href="https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011"
+              target="_blank"
+              rel="noreferrer"
+              className="text-cyan-500 hover:text-cyan-600"
+            >
+              {' '}
+              Udacity{' '}
+            </a>
+            .
           </p>
           <div className="mt-12">
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
