9b9fb922f7de0496b3513dbdbb991c0f93e0672b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-06 09:51:23 -0600

    Added logos

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const Contact = () => (
   <>
@@ -140,24 +141,27 @@ const Contact = () => (
                   </span>
                 </dd>
               </dl>
-              <ul className="mt-8 flex space-x-12" role="list">
+              <ul className="mt-8 flex space-x-12">
                 <li>
-                  <a className="text-white text-gray-100" href="#">
+                  <a
+                    className="text-white text-gray-100"
+                    href="https://github.com/salymk"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="sr-only">GitHub</span>
-                    <svg
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M11.999 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.386.6.11.819-.26.819-.578 0-.284-.01-1.04-.017-2.04-3.337.724-4.042-1.61-4.042-1.61-.545-1.386-1.332-1.755-1.332-1.755-1.09-.744.082-.73.082-.73 1.205.086 1.838 1.238 1.838 1.238 1.07 1.833 2.81 1.304 3.493.996.109-.775.419-1.303.762-1.603C7.145 17 4.343 15.97 4.343 11.373c0-1.31.468-2.382 1.236-3.22-.124-.304-.536-1.524.118-3.176 0 0 1.007-.323 3.3 1.23.956-.266 1.983-.4 3.003-.404 1.02.005 2.046.138 3.005.404 2.29-1.553 3.296-1.23 3.296-1.23.655 1.652.243 2.872.12 3.176.77.838 1.233 1.91 1.233 3.22 0 4.61-2.806 5.624-5.478 5.921.43.37.814 1.103.814 2.223 0 1.603-.015 2.898-.015 3.291 0 .321.217.695.825.578C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12.001-12"
-                        fill="currentColor"
-                      />
-                    </svg>
+                    <FaGithub size="2em" className="hover:text-gray-300" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-white text-gray-100"
+                    href="https://www.linkedin.com/in/salym-akhmedov/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span className="sr-only">LinkedIn</span>
+                    <FaLinkedin size="2em" className="hover:text-gray-300" />
                   </a>
                 </li>
               </ul>
