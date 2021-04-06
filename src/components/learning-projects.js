819151774c3114c7c0272723501f4d7bce58f524
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-06 10:52:18 -0600

    Mapped over the project data and created a card for each project

--- src/components/learning-projects.js
+++ src/components/learning-projects.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { Dialog } from '@reach/dialog'
-import '@reach/dialog/styles.css'
+import bootcampProjects from '../data/bootcampProjects'
 
 const LearningProjects = () => {
   const [showDialog, setShowDialog] = useState(false)
   const open = () => setShowDialog(true)
   const close = () => setShowDialog(false)
+  console.log(bootcampProjects)
   return (
     <>
       <div className="relative bg-white sm:py-8">
@@ -20,102 +20,46 @@ const LearningProjects = () => {
           </p>
           <div className="mt-12">
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-              <div className="pt-6">
-                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                  <div className="-mt-6">
-                    <div className="relative h-40 mb-6">
-                      <img
-                        className="w-full h-full object-cover rounded-lg shadow-xl"
-                        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                        alt=""
-                      />
-                    </div>
-                    <h3 className="mt-2 text-xl font-medium text-gray-900 tracking-tight">
-                      Employee Directory
-                    </h3>
-                    <p className="mt-2 text-base text-cyan-500">HTML/CSS/JS</p>
+              {bootcampProjects.map(project => (
+                <div className="pt-6" key={project.id}>
+                  <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div className="-mt-6">
+                      <div className="relative h-40 mb-6">
+                        <img
+                          className="w-full h-full object-cover rounded-lg shadow-xl"
+                          src={project.image.src}
+                          alt={project.title}
+                        />
+                      </div>
+                      <h3 className="mt-2 text-xl font-medium text-gray-900 tracking-tight">
+                        {project.title}
+                      </h3>
+                      <p className="mt-2 text-base text-cyan-500">
+                        {project.skills}
+                      </p>
 
-                    <div className="mt-6 flex flex-row justify-center space-x-2">
-                      <a
-                        href="#"
-                        className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
-                      >
-                        Live Demo
-                      </a>
-                      <a
-                        href="#"
-                        className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-600 bg-cyan-50 hover:bg-cyan-100"
-                      >
-                        Github
-                      </a>
+                      <div className="mt-6 flex flex-row justify-center space-x-2">
+                        <a
+                          href={project.preview.url}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
+                        >
+                          Live Demo
+                        </a>
+                        <a
+                          href={project.github.url}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-600 bg-cyan-50 hover:bg-cyan-100"
+                        >
+                          Github
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="pt-6">
-                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                  <div className="-mt-6">
-                    <div className="relative h-40 mb-6">
-                      <img
-                        className="w-full h-full object-cover rounded-lg shadow-xl"
-                        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                        alt=""
-                      />
-                    </div>
-                    <h3 className="mt-2 text-xl font-medium text-gray-900 tracking-tight">
-                      Employee Directory
-                    </h3>
-                    <p className="mt-2 text-base text-cyan-500">HTML/CSS/JS</p>
-
-                    <div className="mt-6 flex flex-row justify-center space-x-2">
-                      <a
-                        href="#"
-                        className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
-                      >
-                        Live Demo
-                      </a>
-                      <a
-                        href="#"
-                        className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-600 bg-cyan-50 hover:bg-cyan-100"
-                      >
-                        Github
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="pt-6">
-                <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
-                  <div className="-mt-6">
-                    <div className="relative h-40 mb-6">
-                      <img
-                        className="w-full h-full object-cover rounded-lg shadow-xl"
-                        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-                        alt=""
-                      />
-                    </div>
-                    <h3 className="mt-2 text-xl font-medium text-gray-900 tracking-tight">
-                      Employee Directory
-                    </h3>
-                    <p className="mt-2 text-base text-cyan-500">HTML/CSS/JS</p>
-
-                    <div className="mt-6 flex flex-row justify-center space-x-2">
-                      <a
-                        href="#"
-                        className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-teal-500 to-cyan-600 hover:from-teal-600 hover:to-cyan-700"
-                      >
-                        Live Demo
-                      </a>
-                      <a
-                        href="#"
-                        className="w-1/2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-cyan-600 bg-cyan-50 hover:bg-cyan-100"
-                      >
-                        Github
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
