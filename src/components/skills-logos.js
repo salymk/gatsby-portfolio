4b436dd0d72d9e1ac3712678539b4a415cce1a9b
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-06 09:10:40 -0600

    Changed color of logos and text

--- src/components/skills-logos.js
+++ src/components/skills-logos.js
@@ -24,62 +24,62 @@ const SkillLogos = () => (
         <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">HTML</span>
-              <FaHtml5 size="4em" color="#e34f26" className="mt-2" />
+              <span className="text-sm text-gray-900">HTML</span>
+              <FaHtml5 size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">CSS</span>
-              <FaCss3Alt size="4em" color="#0085bb" className="mt-2" />
+              <span className="text-sm text-gray-900">CSS</span>
+              <FaCss3Alt size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">JavaScript</span>
-              <SiJavascript size="4em" color="yellow" className="mt-2" />
+              <span className="text-sm text-gray-900">JavaScript</span>
+              <SiJavascript size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">ReactJS</span>
-              <FaReact size="4em" color="#0023a2" className="mt-2" />
+              <span className="text-sm text-gray-900">ReactJS</span>
+              <FaReact size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">GatsbyJS</span>
-              <GrGatsbyjs size="4em" color="purple" className="mt-2" />
+              <span className="text-sm text-gray-900">GatsbyJS</span>
+              <GrGatsbyjs size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">TailwindCSS</span>
-              <SiTailwindcss size="4em" color="cyan" className="mt-2" />
+              <span className="text-sm text-gray-900">TailwindCSS</span>
+              <SiTailwindcss size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">Github</span>
-              <FaGithub size="4em" color="black" className="mt-2" />
+              <span className="text-sm text-gray-900">Github</span>
+              <FaGithub size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">Git</span>
-              <DiGit size="4em" color="#f1502f" className="mt-2" />
+              <span className="text-sm text-gray-900">Git</span>
+              <DiGit size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">Vercel</span>
-              <IoLogoVercel size="4em" color="black" className="mt-2" />
+              <span className="text-sm text-gray-900">Vercel</span>
+              <IoLogoVercel size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-600">DigitalOcean</span>
-              <FaDigitalOcean size="4em" color="008bcf" className="mt-2" />
+              <span className="text-sm text-gray-900">DigitalOcean</span>
+              <FaDigitalOcean size="4em" color="#dfdfdf" className="mt-2" />
             </div>
           </div>
         </div>
