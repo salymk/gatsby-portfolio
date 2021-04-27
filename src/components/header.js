3ff289b99e5d05c05719dea1e3bcf18c4b663323
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-26 20:56:44 -0600

    Changed shadow

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { Link as LinkScroll } from 'react-scroll'
 
@@ -6,7 +6,7 @@ const Header = () => (
   <header>
     <Menu
       as="div"
-      className="fixed w-full z-20 top-0 shadow-md md:shadow-none md:relative bg-white "
+      className="fixed w-full z-20 top-0 shadow md:shadow-none md:relative bg-white "
     >
       {({ open }) => (
         <>
@@ -119,12 +119,13 @@ const Header = () => (
           </div>
           <Transition
             show={open}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
+            as={Fragment}
+            enter="duration-200 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
             <Menu.Items
               static
