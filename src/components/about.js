07e1ae7cd5c0bac7f1b4dd6ab7cce35ceb019788
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-30 19:46:47 -0600

    Changed StaticImage to img

--- src/components/about.js
+++ src/components/about.js
@@ -17,12 +17,10 @@ const About = () => (
           />
           <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
             <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
-              <StaticImage
+              <img
                 imgClassName="object-cover lg:h-full lg:w-full rounded-lg"
                 src="../images/salymportimg.jpg"
                 alt="Salym Akhmedov"
-                placeholder="blurred"
-                loading="eager"
               />
             </div>
           </div>
