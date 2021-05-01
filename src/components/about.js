bd3cf5d566661a3cc2655bb64533c09c8f0bc0b2
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-04-30 20:31:20 -0600

    Fixed className

--- src/components/about.js
+++ src/components/about.js
@@ -18,7 +18,7 @@ const About = () => (
           <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
             <div className="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
               <img
-                imgClassName="object-cover lg:h-full lg:w-full rounded-lg"
+                className="object-cover lg:h-full lg:w-full rounded-lg"
                 src="../images/salymportimg.jpg"
                 alt="Salym Akhmedov"
               />
